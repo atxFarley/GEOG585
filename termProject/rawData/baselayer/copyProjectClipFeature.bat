146ee9041d9840111d2b2ead4a7327345c2bf389
atxFarley <afarley@austin.rr.com>
2019-01-08 18:04:08 -0600

    vectorReproject

---
+++ termProject/rawData/baselayer/copyProjectClipFeature.bat
@@ -0,0 +1,15 @@
+@ECHO OFF 
+REM Clip and Project using ogr2ogr
+
+ECHO Navigate to term project data 
+cd /d C:\Users\Farley\iCloudDrive\PSUGIS\GEOG585\termProject\rawData
+ECHO Set ogr2ogr executable path
+set ogr2ogrPath=C:\QGIS3_4\bin\ogr2ogr.exe
+ECHO Set GDAL Data path
+set GDAL_DATA=C:\QGIS3_4\share\gdal
+ECHO %GDAL_DATA%
+
+ECHO reproject features
+%ogr2ogrPath% -skipfailures -s_srs EPSG:4326 -t_srs EPSG:3857 C:\Users\Farley\iCloudDrive\PSUGIS\GEOG585\termProject\rawData\baselayer\reprojected\Texas_County_Boundaries.shp C:\Users\Farley\iCloudDrive\PSUGIS\GEOG585\termProject\rawData\baselayer\raw\Texas_County_Boundaries.shp
+ECHO reprojection complete
+PAUSE
