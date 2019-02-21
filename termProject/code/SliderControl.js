6f5074369a8c4ef3cd8a7bdd5ab109c87ab6511a
atxFarley <afarley@austin.rr.com>
2019-02-21 14:46:04 -0600

    changes for final project

--- termProject/code/SliderControl.js
+++ termProject/code/SliderControl.js
@@ -10,6 +10,7 @@ L.Control.SliderControl = L.Control.extend({
         minValue: 0,
         showAllOnStart: true,
         markers: null,
+        labels: null,
         range: false,
         follow: false,
         sameDate: false,
@@ -21,6 +22,7 @@ L.Control.SliderControl = L.Control.extend({
         this._layer = this.options.layer;
     },
     extractTimestamp: function (time, options) {
+        console.log("extractTimestamp: time: " + time);
         if (options.isEpoch) {
             time = (new Date(parseInt(time))).toString(); // this is local time
         }
@@ -43,7 +45,7 @@ L.Control.SliderControl = L.Control.extend({
         this.options.map = map;
         // Create a control sliderContainer with a jquery ui slider
         var sliderContainer = L.DomUtil.create('div', 'slider', this._container);
-        $(sliderContainer).append('<div id="leaflet-slider" style="width:200px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:200px; margin-top:13px; background-color:#FFFFFF; text-align:center; border-radius:5px;"></div></div>');
+        $(sliderContainer).append('<div id="leaflet-slider" class="leaflet-slider"><div id="slider-timestamp" class="ui-slider-label"></div><div class="ui-slider-handle"></div></div>');
         //Prevent map panning/zooming while using the slider
         $(sliderContainer).mousedown(function () {
             map.dragging.disable();
@@ -59,6 +61,7 @@ L.Control.SliderControl = L.Control.extend({
         this.options.markers = [];
         //If a layer has been provided: calculate the min and max values for the slider
         if (this._layer) {
+            console.log("layer has been provided");
             var index_temp = 0;
             this._layer.eachLayer(function (layer) {
                 options.markers[index_temp] = layer;
@@ -66,8 +69,12 @@ L.Control.SliderControl = L.Control.extend({
             });
             options.maxValue = index_temp - 1;
             this.options = options;
+            console.log("min: " + this.options.minValue);
+            console.log("max: " + this.options.maxValue);
+            console.log("labels: " + this.options.labels);
+
         } else {
-            console.log("Error: You have to specify a layer via new SliderControl({layer: your_layer});");
+            console.error("Error: You have to specify a layer via new SliderControl({layer: your_layer});");
         }
         return sliderContainer;
     },
@@ -84,7 +91,7 @@ L.Control.SliderControl = L.Control.extend({
     startSlider: function () {
         try {
             _options = this.options;
-            _extractTimestamp = this.extractTimestamp
+            _extractTimestamp = this.extractTimestamp;
             var index_start = _options.minValue;
             if (_options.showAllOnStart) {
                 index_start = _options.maxValue;
@@ -97,7 +104,7 @@ L.Control.SliderControl = L.Control.extend({
             console.log("index_start: " + index_start);
             console.log("minValue: " + _options.minValue);
             console.log("maxValue: " + _options.maxValue);
-            
+
             $("#leaflet-slider").slider({
                 range: _options.range,
                 value: _options.value,
@@ -106,6 +113,31 @@ L.Control.SliderControl = L.Control.extend({
                 max: _options.maxValue,
                 sameDate: _options.sameDate,
                 step: 1,
+                create: function (e, ui) {
+                    try {
+                        console.log("create(): ");
+                        var mx = _options.maxValue;
+                        var mn = _options.minValue;
+                        console.log("Max: " + mx + ", min: " + mn);
+                        var $slider = $('#leaflet-slider');
+                        var spacing = 100 / (mx - mn);
+                        $slider.find('.ui-slider-tick-mark').remove();
+                        var labelIndex = 0;
+                        for (var i = 0; i < mx - mn; i++) {
+                            var tickLabel = _options.labels[labelIndex];
+                            console.log("tickLabel: " + tickLabel);
+                            labelIndex++;
+                            $('<span class="ui-slider-tick-mark">' + tickLabel + '</span>').css('left', (spacing * i) + '%').appendTo($slider);
+                        }
+                        //get last label from array
+                        var tickLabel = _options.labels[labelIndex];
+                        console.log("tickLabel: " + tickLabel);
+                        labelIndex++;
+                        $('<span class="ui-slider-tick-mark">' + tickLabel + '</span>').css('left', (spacing * i) + '%').appendTo($slider);
+                    } catch (e) {
+                        console.error("error caught setting tick marks: " + e);
+                    }
+                },
                 slide: function (e, ui) {
                     console.log("slide()");
                     console.log("ui.value: " + ui.value);
@@ -165,8 +197,9 @@ L.Control.SliderControl = L.Control.extend({
                                 currentTime = _options.markers[ui.value].options.time;
                             }
                             for (i = _options.minValue; i <= _options.maxValue; i++) {
-                                if (_options.markers[i].options.time == currentTime)
+                                if (_options.markers[i].options.time === currentTime) {
                                     map.addLayer(_options.markers[i]);
+                                }
                             }
                         } else {
                             for (i = _options.minValue; i <= ui.value; i++) {
@@ -185,19 +218,25 @@ L.Control.SliderControl = L.Control.extend({
                     }
                 }
             });
+//             for (i = _options.minValue; i <= index_start; i++) {
+//                console.log("adding layer: " + _options.markers[i]);
+//                _options.map.addLayer(_options.markers[i]);
+//            }
+            _options.map.addLayer(_options.markers[index_start]);
             if (!_options.range && _options.alwaysShowDate) {
-                var year; 
+                console.log("setting year in label");
+                var year;
                 try {
-                   year = _extractTimestamp(_options.markers[index_start].feature.properties[_options.timeAttribute], _options);                
+                    console.log("index_start: " + index_start);
+                    console.log("_options.markers[index_start]: " + _options.markers[index_start]);
+                    year = _extractTimestamp(_options.markers[index_start].feature.properties[_options.timeAttribute], _options);
                 } catch (e) {
                     console.error("exception caught setting slider-timestamp: " + e);
                     year = "2017";
                 }
-                   $('#slider-timestamp').html(year);
+                $('#slider-timestamp').html(year);
             }
-            for (i = _options.minValue; i <= index_start; i++) {
-                _options.map.addLayer(_options.markers[i]);
-            }
+
         } catch (e) {
             console.error("Error caught starting slider: " + e);
         }
